24c114ca23357a4d1b03244cc40acd3fa0921664
HeinWeb <h.sithu319@gmail.com>
2024-12-15 17:54:58 +0630

    second commit and finished

--- main.js
+++ main.js
@@ -4,19 +4,21 @@ const img2 = document.querySelector("#img-2");
 
 window.addEventListener("scroll",image1);
 function image1 (){
-    if(window.scrollY > 150){
+    if(window.scrollY >= 300){
         img1.style.transform = "translateY(-300px)";
     }
-    else if(window.scrollY > 100){
+    else if(window.scrollY >= 200){
         img1.style.transform = "translateY(-250px)";
     }
-    else if(window.scrollY > 50){
-        img1.style.transform = "translateY(-200px)";
+    else if(window.scrollY >= 100){
+        img1.style.transform = "translateY(-150px)";
     }
     else{
-        img1.style.transform = "translateY(-180px)";
+        img1.style.transform = "translateY(-80px)";
     }
 }
+
+
 
 // Image 4,5,6 slide
 const showImg =[
@@ -39,3 +41,41 @@ function image2 (){
         img2.src = orgSrc;
     }
 }
+
+
+// Faq Toggle
+const faq = document.querySelectorAll(".faq");
+
+for(let faqItem of faq){
+    faqItem.addEventListener("click", () => {
+        faqItem.classList.toggle("show");
+    })
+}
+
+// Scroll Top Button
+const scrollTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () =>{
+    let scroll = this.scrollY;
+    console.log(scroll)
+    if(scroll > 9500){
+        scrollTop.style.backgroundColor = "white";
+        scrollTop.style.color = "black";
+    }
+    else if(scroll > 300){
+        scrollTop.style.display = "grid";
+    }
+    else{
+        scrollTop.style.display = "none";
+    };
+
+    if(scroll < 9490){
+        scrollTop.style.backgroundColor = "black";
+        scrollTop.style.color = "white";
+    }
+})
+
+scrollTop.addEventListener("click", () =>{
+    window.scrollTo({top: 0, behavior: "smooth"})
+})
+
